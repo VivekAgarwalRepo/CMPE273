0dc47523f301dfc9c21d3cabe620c28f1cc111b9
VivekAgarwalRepo <vivek.d.agarwal@sjsu.edu>
2016-09-17 14:44:31 -0700

    Delete variables.js

--- JavaScript/Basic/variables.js
+++
@@ -1,9 +0,0 @@
-concat(str)
-{
-
-var first=document.getElementById('fname').value;
-var last=document.getElementById('lname').value;
-var result=document.getElementById('output');
-result.innerHTML=str;
-}
-
